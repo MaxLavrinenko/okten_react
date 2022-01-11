210f5724114e45d9175522edecd67f9d15efd717
MaxLavrinenko <79162671+MaxLavrinenko@users.noreply.github.com>
2022-01-11 22:40:21 +0200

    cw1

--- src/components/Launch/Launch.js
+++ src/components/Launch/Launch.js
@@ -1,11 +1,11 @@
 import './Launch.css'
 
-const Launch =(props)=>{
-    const {mission_name,launch_year,mission_patch_small} = props
-    return(
+const Launch = (props) => {
+    const {mission_name, launch_year, mission_patch_small} = props
+    return (
         <div className={'container'}>
             <div>
-            <h4>Mission name:{mission_name}</h4>
+                <h4>Mission name:{mission_name}</h4>
                 <h5>Launch Year: {launch_year}</h5>
             </div>
             <img src={mission_patch_small} alt={'ddd'}/>
--- src/components/Launches/Launches.js
+++ src/components/Launches/Launches.js
@@ -1,21 +1,24 @@
-import {useEffect,useState} from "react";
+import {useEffect, useState} from "react";
+
 import Launch from "../Launch/Launch";
-const Launches = ()=>{
-    const [launches,setLaunches] = useState([]);
-    let counter=0
-    useEffect(()=>{
+
+
+const Launches = () => {
+    const [launches, setLaunches] = useState([]);
+
+    useEffect(() => {
         fetch('https://api.spacexdata.com/v3/launches/')
             .then(value => value.json())
-            .then(value => setLaunches(value.filter(value=>value.launch_year < 2020)))
-    },[]);
+            .then(value => setLaunches(value.filter(value => value.launch_year < 2020)))
+    }, []);
 
-    return(
+    return (
         <div>
-            {launches.map(value => <Launch key = {counter++}
-                                      mission_name={value.mission_name}
+            {launches.map(value => <Launch key={value.flight_number}
+                                           mission_name={value.mission_name}
                                            launch_year={value.launch_year}
                                            mission_patch_small={value.links.mission_patch_small}
-            /> )}
+            />)}
         </div>
     )
 }
