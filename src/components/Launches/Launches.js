b4f7e8cb5891d28f60d3640ed3f4ff59156cadcf
MaxLavrinenko <79162671+MaxLavrinenko@users.noreply.github.com>
2022-01-11 18:59:07 +0200

    cw1

--- src/components/Launches/Launches.js
+++ src/components/Launches/Launches.js
@@ -2,14 +2,13 @@ import {useEffect,useState} from "react";
 import Launch from "../Launch/Launch";
 const Launches = ()=>{
     const [launches,setLaunches] = useState([]);
-    let res = []
     let counter=0
     useEffect(()=>{
         fetch('https://api.spacexdata.com/v3/launches/')
             .then(value => value.json())
             .then(value => setLaunches(value))
     },[]);
-    res = launches.filter(item => item.launch_year < 2020)
+    let res = launches.filter(item => item.launch_year < 2020)
 
     return(
         <div>
