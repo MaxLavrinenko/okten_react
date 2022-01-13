7116408d31dd045493b5b77ce63812b2b28df7a5
MaxLavrinenko <79162671+MaxLavrinenko@users.noreply.github.com>
2022-01-13 23:10:38 +0200

    cw2

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -7,25 +7,24 @@ import User from "../User/User";
 const Users = () => {
 
     const [users, setUsers] = useState([])
-    const [res, setRes] = useState([])
     const [form, setForm] = useState({name: '', username: '', email: ''})
-
+    const [filter, setFilter] = useState([]);
     useEffect(() => {
         userServices.getAll()
-            .then(value => setUsers(value))
+            .then(value => {setFilter(value);setUsers(value)})
     }, [])
 
     const find = (e) => {
         e.preventDefault()
+        setFilter(
         users.filter(value => {
-            if (value.name.includes(form.name)) {
-                // setRes(res.push(value))
-                console.log(res);
-            }
-        })
-        console.log(res);
+            return (value.name.toLowerCase().includes(form.name) &&
+                value.username.toLowerCase().includes(form.username) &&
+                value.email.toLowerCase().includes(form.email));
+
+        }))
     }
-    const formHendler = (e) => {
+    const formHandler = (e) => {
         const keyName = e.target.name;
         setForm({...form, [keyName]: e.target.value})
     }
@@ -34,17 +33,14 @@ const Users = () => {
         <div>
             <div>
                 <form onSubmit={find}>
-                    Name: <input type='text' name={'name'} onChange={formHendler}/>
-                    User name: <input type='text' name={'username'} onChange={formHendler}/>
-                    Email: <input type='text' name={'email'} onChange={formHendler}/>
+                    Name: <input type='text' name={'name'} onChange={formHandler}/>
+                    User name: <input type='text' name={'username'} onChange={formHandler}/>
+                    Email: <input type='text' name={'email'} onChange={formHandler}/>
                     <button>Find</button>
                 </form>
             </div>
             <div>
-                {res && res.map(value =><User key={value.id}
-                                          value={value}/> )}
-                {users.map(value => <User key={value.id}
-                                          value={value}/>)}
+                {filter.map(value => <User key={value.id} value={value}/>)}
             </div>
         </div>
     );
