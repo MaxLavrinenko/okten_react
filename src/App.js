775e8b77c1b669fc83cb64c9dfeff37f380891b8
MaxLavrinenko <79162671+MaxLavrinenko@users.noreply.github.com>
2022-01-30 15:06:09 +0200

    cw4_reducer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+            <form>
+                <label>Add Cat: <input ref={catInput} type="text" name={'cat'}/></label>
+                <button onClick={addCat}>Save</button>
+                <label>Add Dog: <input ref={dogInput} type="text" name={'dog'}/></label>
+                <button onClick={addDog}>Save</button>
+            </form>
+            <h1>
+                <hr/>
+            </h1>
+            <div style={{display: 'flex', justifyContent: 'center', gap: '500px'}}>
+                <Cats cats={state.cats} deleteCat={deleteCat}/>
+                <Dogs dogs={state.dogs} deleteDog={deleteDog}/>
+            </div>
     </div>
   );
 }
